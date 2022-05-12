b79565caa7677e8ad6a38170f6ccdfc5cbf54a42
kanya384 <kanya384@mail.ru>
2022-05-12 09:57:01 +0300

    added maps

--- src/components/get_there.js
+++ src/components/get_there.js
@@ -1,14 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 
 export const GetThere = () => {
+    const [type, setType] = useState(0)
+    
     return (
         <div class="get_there">
             <div class="tm tt"><b>Как добраться</b></div>
             <div class="get_there__nav">
-                <div>На автомобиле</div>
-                <div class="act">На общественном транспорте</div>
+                <div onClick={()=>setType(0)} className={type==0?"act":""}>На общественном транспорте</div>
+                <div onClick={()=>setType(1)} className={type==1?"act":""}>На автомобиле</div>
             </div>
-            <div class="get_there__map"><img src="img/get_there__map.jpg" /></div>
+            <div className="get_there__map">
+                {type == 0 ? <div style={{ position: "relative", overflow: "hidden", width:"100%" }}><iframe  src="https://yandex.ru/map-widget/v1/-/CCUFbWFh1D" width="100%" height="480" frameborder="0" allowfullscreen="true" style={{ position: "relative" }}></iframe></div> : ""}
+                {type == 1 ? <div style={{ position: "relative", overflow: "hidden", width:"100%" }}><iframe src="https://yandex.ru/map-widget/v1/-/CCUFbWSE1B" width="100%" height="480" frameborder="0" allowfullscreen="true" style={{ position: "relative" }}></iframe></div> : ""}
+            </div>
         </div>
     )
 }
